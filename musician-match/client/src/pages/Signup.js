96a740feb2d19bbadb4c7957ff904113c62b316e
mikestem <michael.a.stem@gmail.com>
2023-05-03 22:10:26 -0400

    added age and instrument to signup

--- musician-match/client/src/pages/Signup.js
+++ musician-match/client/src/pages/Signup.js
@@ -11,6 +11,8 @@ const Signup = () => {
     name: '',
     email: '',
     password: '',
+    age: '',
+    instrument: ''
   });
   const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
 
