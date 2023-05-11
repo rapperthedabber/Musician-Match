d54e5c747b9050265c02cc362c49e5e52c4188ce
My Tang <my@Mys-MacBook-Air.local>
2023-05-11 19:22:19 +0700

    removed routes and controller as they are not needed

--- musician-match/server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const chatRoomRoutes = require('./chatRoomRoutes');
-const chatMessageRoutes = require('./chatMessageRoutes');
-
-router.use('/chatRooms', chatRoomRoutes);
-router.use('/chatMessages', chatMessageRoutes);
-
-module.exports = router;
